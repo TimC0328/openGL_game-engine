05198d66012f001c5937d9c66ced6e5d47132d08
TimC0328 <TimC0328@gmail.com>
2021-06-21 00:02:27 -0400

    Added shaders

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "shader.h"
 #include "mesh.h"
 
 int main()
@@ -12,10 +13,14 @@ int main()
                           Vertex(glm::vec3(0.5, -0.5, 0)), };
 
     Mesh mesh(vertices, sizeof(vertices)/sizeof(vertices[0]));
+    Shader shader("./res/basicShader");
 
     while(!display.IsClosed())
     {
         display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+
+        shader.Bind();
+        mesh.Draw();
 
         display.Update();
     }
